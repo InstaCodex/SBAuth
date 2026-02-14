e054fd76abaea0a4944b23746f1dd6ddb7780963
InstaCodex <kevinmitnick@gmail.com>
2026-02-14 20:25:25 +0700

    buat lebih simple aja sidebarnya soale atek bootstrap lawas anjer

--- resources/views/components/sb-admin-sidebar.blade.php
+++ resources/views/components/sb-admin-sidebar.blade.php
@@ -12,10 +12,12 @@
     <hr class="sidebar-divider my-0">
 
     <!-- Nav Item - Dashboard -->
-    <x-sb-nav-link href="{{ route('dashboard') }}" :aktif="request()->routeIs('dashboard')">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
-        <span>Dashboard</span>
-    </x-sb-nav-link>
+    <li class="nav-item {{ request()->routeIs('dashboard') ? 'active' : '' }}">
+        <a class="nav-link" href="{{ route('dashboard') }}">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+        </a>
+    </li>
 
     <!-- Divider -->
     <hr class="sidebar-divider">
