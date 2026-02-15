b05b3b3d1c812bf95f1db78f1d621af063298fdb
InstaCodex <kevinmitnick@gmail.com>
2026-02-15 22:05:32 +0700

    benerin sidebar lagi dan lagi bingung

--- resources/views/components/sb-admin-sidebar.blade.php
+++ resources/views/components/sb-admin-sidebar.blade.php
@@ -28,7 +28,7 @@
     </div>
 
     <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
+    <li class="nav-item {{ request()->routeIs('*component*') ? 'active' : '' }}">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
             aria-expanded="true" aria-controls="collapseTwo">
             <i class="fas fa-fw fa-cog"></i>
